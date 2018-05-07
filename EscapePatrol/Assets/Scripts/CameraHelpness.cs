e9372932c5cf4c159eabedb084f1a17851bb4b84
延泽 许 <peter13994@126.com>
2018-05-07 19:30:03 +0800

    commit

---
+++ EscapePatrol/Assets/Scripts/CameraHelpness.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHelpness : MonoBehaviour
+{
+    public GameObject follow;            //跟随的物体
+    public float smothing = 5f;          //相机跟随的速度
+    public Vector3 offset;                      //相机与物体相对偏移位置
+    public FirstSceneController first = (FirstSceneController)SSDirector.GetInstance().CurrentScenceController;
+
+    void Start()
+    {
+        offset = transform.position - follow.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 target = follow.transform.position + offset;
+        //摄像机自身位置到目标位置平滑过渡
+        transform.position = Vector3.Lerp(transform.position, target, smothing * Time.deltaTime);
+    }
+}
---
+++ EscapePatrol/Assets/Scripts/PatrolCollider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolCollider : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            //print("enter");
+            //玩家进入侦察兵追捕范围
+            this.gameObject.transform.parent.GetComponent<PatrolData>().follow_player = true;
+            this.gameObject.transform.parent.GetComponent<PatrolData>().player = collider.gameObject;
+        }
+    }
+    private void OnGUI()
+    {
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 40;
+        style.normal.textColor = Color.red;
+        if(this.gameObject.transform.parent.GetComponent<PatrolData>().follow_player == true)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 150, Screen.height / 2 - 200, 400, 500), "!!You Have Been Found!!", style);
+        }
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            this.gameObject.transform.parent.GetComponent<PatrolData>().follow_player = false;
+            this.gameObject.transform.parent.GetComponent<PatrolData>().player = null;
+        }
+    }
+}
